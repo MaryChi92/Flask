a413126b29699e6e175f58b39e3337e964cb84f0
MaryChi92 <pereverzeva.mary@gmail.com>
2023-04-11 21:28:14 +0300

    lesson7 files added

--- blog/article/views.py
+++ blog/article/views.py
@@ -1,10 +1,11 @@
 from flask import render_template, Blueprint, request, redirect, url_for
 from flask_login import login_required, current_user
+from sqlalchemy.orm import joinedload
 from werkzeug.exceptions import NotFound
 
 from blog.extensions import db
 from blog.forms.article import ArticleCreateForm
-from blog.models import Article, Author
+from blog.models import Article, Author, Tag
 
 articles = Blueprint('articles', __name__, url_prefix='/articles', static_folder='../static')
 
@@ -15,9 +16,18 @@ def article_list():
     return render_template('articles/list.html', articles=_articles)
 
 
+@articles.route('/<string:tag_name>/', endpoint='filter', methods=['GET'])
+def article_list_filtered(tag_name: str):
+    _articles: Article = Article.query.options(joinedload(Article.tags)).filter(Article.tags.any(Tag.name == tag_name))
+    return render_template('articles/list.html', articles=_articles)
+
+
 @articles.route('/<int:article_id>/', endpoint='details', methods=['GET'])
 def article_details(article_id: int):
-    _article: Article = Article.query.filter_by(id=article_id).one_or_none()
+    _article: Article = Article.query.filter_by(id=article_id)\
+        .options(joinedload(Article.tags))\
+        .one_or_none()
+
     if _article is None:
         raise NotFound(f"Article {article_id} doesn't exist!")
     return render_template('articles/details.html', article=_article)
@@ -27,6 +37,7 @@ def article_details(article_id: int):
 @login_required
 def create_article_form():
     form = ArticleCreateForm(request.form)
+    form.tags.choices = [(tag.id, tag.name) for tag in Tag.query.order_by('name')]
     return render_template('articles/create.html', form=form)
 
 
@@ -34,8 +45,11 @@ def create_article_form():
 @login_required
 def create_article():
     form = ArticleCreateForm(request.form)
+    form.tags.choices = [(tag.id, tag.name) for tag in Tag.query.order_by('name')]
+
     if form.validate_on_submit():
         _article = Article(title=form.title.data.strip(), body=form.text.data)
+
         if current_user.author:
             _article.author_id = current_user.author.id
         else:
@@ -43,6 +57,11 @@ def create_article():
             db.session.add(author)
             db.session.flush()
             _article.author_id = author.id
+
+        if form.tags.data:
+            selected_tags = Tag.query.filter(Tag.id.in_(form.tags.data))
+            for tag in selected_tags:
+                _article.tags.append(tag)
 
         db.session.add(_article)
         db.session.commit()
--- blog/author/views.py
+++ blog/author/views.py
@@ -12,13 +12,3 @@ authors = Blueprint('authors', __name__, url_prefix='/authors', static_folder='.
 def authors_list():
     _authors = Author.query.all()
     return render_template('authors/list.html', authors=_authors)
-
-
-# @authors.route('/<int:pk>', endpoint='details')
-# @login_required
-# def author_details(pk: int):
-#     selected_author = User.query.filter_by(id=pk).one_or_none()
-#     articles = article_views.ARTICLES
-#     if not selected_user:
-#         raise NotFound(f"User {pk} doesn't exist!")
-#     return render_template('users/details.html', user=selected_user, articles=articles)
